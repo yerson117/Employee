556cca05c2ca0b983f6aacac919444d3136928bb
Yerson <yersonsuarez@usantotomas.edu.co>
2021-11-19 22:32:38 -0500

    creación metodos back

--- controllers/employee.controller.js
+++ controllers/employee.controller.js
@@ -19,4 +19,62 @@ ctrlEmployee.create = async (req, res) =>{
         status: true
     });
 };
+
+
+ctrlEmployee.list = async (req, res) =>{
+    const employees = await Employee.find({})
+    res.json(employees);
+};
+
+
+ctrlEmployee.update = async (req, res) =>{
+     const {_id, name, phone, email, document, salary, gender,dateofbirth} = req.body;
+     await Employee.findOneAndUpdate(
+         { _id: _id },
+         {
+             name: name,
+             phone: phone,
+             email: email,
+             document: document,
+             salary: salary,
+             gender: gender,
+             dateofbirth: dateofbirth
+         }
+     );
+     res.json({status: true})
+};
+
+ctrlEmployee.delete = async (req, res) =>{
+    const{ _id } = req.params;
+    const employees = await Employee.deleteOne({ _id: _id});
+    res.json({ status: true});
+};
+
+
+ctrlEmployee.search = async (req, res) => {
+    console.log(req.body.name)
+    const name = await Employee.find( { employee :
+        { $regex: ".*" +req.body.name + ".*"}
+    })
+    res.json(name)
+};
+
+ctrlEmployee.searchget = async (req, res ) => {
+    console.log(req.params.name)
+    const name = await Employee.find( { name : { $regex: ".*" + req.params.name + ".*" } })
+    res.json(name)
+};
+
+ctrlEmployee.active = async (req, res) => {
+    console.log(req.body)
+    const { _id, status } = req.body;
+    await Employee.findOneAndUpdate(
+      { _id: _id },
+      { 
+        status: status
+      }
+    );
+    res.json({ status: true });
+  };
+
 module.exports = ctrlEmployee;
--- router/employee.route.js
+++ router/employee.route.js
@@ -3,5 +3,10 @@ router = express.Router(),
 employeectrl = require('../controllers/employee.controller')
 
 router.post('/', employeectrl.create)
-
+router.get('/', employeectrl.list)
+router.put('/', employeectrl.update)
+router.delete('/:_id', employeectrl.delete)
+router.post('/search', employeectrl.search)
+router.get('/searchget', employeectrl.searchget)
+router.put('/active', employeectrl.active)
 module.exports = router
